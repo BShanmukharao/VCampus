97a3c5e758926970972485bb691c01c471ebf960
BShanmukharao <barrishanmukha28@gmail.com>
2024-09-18 19:59:38 +0530

    added

--- src/Components/StudentManagement/DataEntry/NewAdmissions/NewStudentsTransportation/DefineVehiclesForNewStudents/DefineVehiclesForNewStudents.js
+++ src/Components/StudentManagement/DataEntry/NewAdmissions/NewStudentsTransportation/DefineVehiclesForNewStudents/DefineVehiclesForNewStudents.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import { Modal } from 'react-bootstrap';
 
@@ -7,16 +8,28 @@ import './DefineVehiclesForNewStudents.css'
 
 function DefineVehiclesForNewStudents() {
 
-    const [show, setShow] = useState(false);
+    const [showAddPopUp, setshowAddPopUp] = useState(false)
+    const [showUpdatePopUp, setshowUpdatePopUp] = useState(false)
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleAddPopUpClose = () => setshowAddPopUp(false);
+    const handleAddPopUpShow = () => setshowAddPopUp(true);
+
+    const handleUpdatePopUpClose = () => setshowUpdatePopUp(false);
+    const handleUpdatePopUpShow = () => setshowUpdatePopUp(true);
+
 
     /*useEffect(() => {
         const getAreasMasterData = async () => {
+            const apiKey = 'ASCTEST';
+            const URL = `https://visualcampus.in/API/api/VCSync/PostListArea_Mas?key=${apiKey}`;
+            const OPTION = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
             try {
-                const AREASMASTERURL = "https://visualcampus.in/API/api/VCSync/PostListArea_Mas?Key=ASCTEST"
-                const response = await fetch(AREASMASTERURL)
+                const response = await fetch(URL, OPTION)
                 console.log(response)
             } catch (err) {
                 console.log(err.message);
@@ -130,8 +143,8 @@ function DefineVehiclesForNewStudents() {
                     </div>
                     <div className='define-vehicles-for-new-students-buttons-container pt-4 pb-4'>
                         <div>
-                            <button className='define-bus-stops-of-new-students-buttons btn btn-primary' type='button' onClick={handleShow}>ADD</button>
-                            <button className='define-bus-stops-of-new-students-buttons btn btn-secondary' type='submit'>EDIT</button>
+                            <button className='define-bus-stops-of-new-students-buttons btn btn-primary' type='button' onClick={handleAddPopUpShow}>ADD</button>
+                            <button className='define-bus-stops-of-new-students-buttons btn btn-secondary' type='submit' onClick={handleUpdatePopUpShow}>EDIT</button>
                             <button className='define-bus-stops-of-new-students-buttons btn btn-danger' type='button'>DELETE</button>
                             <button className='define-bus-stops-of-new-students-buttons btn btn-success' type='button'>SAVE</button>
                             <button className='define-bus-stops-of-new-students-buttons btn btn-info' type='button'>HELP</button>
@@ -139,53 +152,99 @@ function DefineVehiclesForNewStudents() {
                     </div>
                 </div>
             </div>
-            <div>
-                <Modal show={show} onHide={handleClose}>
-                    <form onSubmit={getPopUpFormData}>
-                        <Modal.Header closeButton>
-                            <Modal.Title className='pop-up-define-vehicles-for-new-students-main-heading'>ADD NEW AREAS MASTER</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <div className='mb-2'>
-                                <label className='pop-up-define-vehicles-for-new-students-label mb-1'>VEHICLE TYPE</label><br />
-                                <select className='pop-up-define-vehicles-for-new-students-input-filed' >
-                                    <option value="ROUTE NO.1">ROUTE NO.1</option>
-                                    <option value="ROUTE NO.2">ROUTE NO.2</option>
-                                    <option value="ROUTE NO.3">ROUTE NO.3</option>
-                                </select>
-                            </div>
-                            <div className='mb-2'>
-                                <label className='pop-up-define-vehicles-for-new-students-label mb-1'>VEHICLE NO</label><br />
-                                <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
-                            </div>
-                            <div className='mb-2'>
-                                <label className='pop-up-define-vehicles-for-new-students-label mb-1'>AREA CODE</label><br />
-                                <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
-                            </div>
-                            <div className='mb-2'>
-                                <label className='pop-up-define-vehicles-for-new-students-label mb-1'>AREA[STOP] NAME</label><br />
-                                <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
-                            </div>
-                            <div className='mb-2'>
-                                <label className='pop-up-define-vehicles-for-new-students-label mb-1'>KILOMETERS</label><br />
-                                <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
-                            </div>
-                            <div className='mb-2'>
-                                <label className='pop-up-define-vehicles-for-new-students-label mb-1'>FARE-RS</label><br />
-                                <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
-                            </div>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="success" type='submit' className='define-bus-stops-of-new-students-buttons' onClick={handleClose}>
-                                SAVE
-                            </Button>
-                            <Button variant="danger" className='define-bus-stops-of-new-students-buttons' onClick={handleClose}>
-                                CANCEL
-                            </Button>
-                        </Modal.Footer>
-                    </form>
-                </Modal>
-            </div>
+
+            <Modal show={showAddPopUp} onHide={handleAddPopUpClose}>
+                <form onSubmit={getPopUpFormData}>
+                    <Modal.Header closeButton>
+                        <Modal.Title className='pop-up-define-vehicles-for-new-students-main-heading'>ADD NEW AREAS MASTER</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className='mb-2'>
+                            <label className='pop-up-define-vehicles-for-new-students-label mb-1'>VEHICLE TYPE</label><br />
+                            <select className='pop-up-define-vehicles-for-new-students-input-filed' >
+                                <option value="ROUTE NO.1">ROUTE NO.1</option>
+                                <option value="ROUTE NO.2">ROUTE NO.2</option>
+                                <option value="ROUTE NO.3">ROUTE NO.3</option>
+                            </select>
+                        </div>
+                        <div className='mb-2'>
+                            <label className='pop-up-define-vehicles-for-new-students-label mb-1'>VEHICLE NO</label><br />
+                            <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
+                        </div>
+                        <div className='mb-2'>
+                            <label className='pop-up-define-vehicles-for-new-students-label mb-1'>AREA CODE</label><br />
+                            <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
+                        </div>
+                        <div className='mb-2'>
+                            <label className='pop-up-define-vehicles-for-new-students-label mb-1'>AREA[STOP] NAME</label><br />
+                            <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
+                        </div>
+                        <div className='mb-2'>
+                            <label className='pop-up-define-vehicles-for-new-students-label mb-1'>KILOMETERS</label><br />
+                            <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
+                        </div>
+                        <div className='mb-2'>
+                            <label className='pop-up-define-vehicles-for-new-students-label mb-1'>FARE-RS</label><br />
+                            <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="success" type='submit' className='define-bus-stops-of-new-students-buttons' onClick={handleAddPopUpClose}>
+                            SAVE
+                        </Button>
+                        <Button variant="danger" className='define-bus-stops-of-new-students-buttons' onClick={handleAddPopUpClose}>
+                            CANCEL
+                        </Button>
+                    </Modal.Footer>
+                </form>
+            </Modal>
+
+            <Modal show={showUpdatePopUp} onHide={setshowUpdatePopUp}>
+                <form onSubmit={getPopUpFormData}>
+                    <Modal.Header closeButton>
+                        <Modal.Title className='pop-up-define-vehicles-for-new-students-main-heading'>EDIT AREAS MASTER</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className='mb-2'>
+                            <label className='pop-up-define-vehicles-for-new-students-label mb-1'>VEHICLE TYPE</label><br />
+                            <select className='pop-up-define-vehicles-for-new-students-input-filed' >
+                                <option value="ROUTE NO.1">ROUTE NO.1</option>
+                                <option value="ROUTE NO.2">ROUTE NO.2</option>
+                                <option value="ROUTE NO.3">ROUTE NO.3</option>
+                            </select>
+                        </div>
+                        <div className='mb-2'>
+                            <label className='pop-up-define-vehicles-for-new-students-label mb-1'>VEHICLE NO</label><br />
+                            <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
+                        </div>
+                        <div className='mb-2'>
+                            <label className='pop-up-define-vehicles-for-new-students-label mb-1'>AREA CODE</label><br />
+                            <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
+                        </div>
+                        <div className='mb-2'>
+                            <label className='pop-up-define-vehicles-for-new-students-label mb-1'>AREA[STOP] NAME</label><br />
+                            <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
+                        </div>
+                        <div className='mb-2'>
+                            <label className='pop-up-define-vehicles-for-new-students-label mb-1'>KILOMETERS</label><br />
+                            <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
+                        </div>
+                        <div className='mb-2'>
+                            <label className='pop-up-define-vehicles-for-new-students-label mb-1'>FARE-RS</label><br />
+                            <input type='text' className='pop-up-define-vehicles-for-new-students-input-filed' />
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="success" type='submit' className='define-bus-stops-of-new-students-buttons' onClick={handleUpdatePopUpClose}>
+                            SAVE
+                        </Button>
+                        <Button variant="danger" className='define-bus-stops-of-new-students-buttons' onClick={handleUpdatePopUpClose}>
+                            CANCEL
+                        </Button>
+                    </Modal.Footer>
+                </form>
+            </Modal>
+
         </>
     )
 }
